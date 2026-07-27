faaeec800db81ff905da72d6bc2b86250754e6cc
tad551216-rgb <tad551216@gmail.com>
2026-07-27 17:17:23 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'day-kiroku-v1';
+const CACHE = 'day-kiroku-v2';
 const ASSETS = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./icon-180.png'];
 self.addEventListener('install', e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
